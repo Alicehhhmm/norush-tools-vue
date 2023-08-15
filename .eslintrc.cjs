65b05d319f36bdf829e2bf28f26add8503bcdbea
Alicehhhmm <1245500390@qq.com>
2023-08-15 18:13:32 +0800

    fix:eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,22 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  env: {
-    // 浏览器环境
-    browser: true,
-    // Node环境
-    node: true,
-    // 启用除了modules以外的所有 ECMAScript 6 特性
-    es2021: true
-  },
   root: true,
+  env: {
+    browser: true, // 浏览器环境
+    node: true, // Node环境
+    es2021: true // 启用除了modules以外的所有 ECMAScript 6 特性
+  },
+  parserOptions: {
+    // Parser that checks the content of the <script> tag
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['@typescript-eslint'],
   extends: [
     './.eslintrc-auto-import.json', // 引入自动化import配置
     'plugin:vue/vue3-essential', //ESLint Vue3 插件扩展
@@ -28,6 +35,35 @@ module.exports = {
   },
   rules: {
     semi: ['warn', 'never'], // 禁止尾部使用分号
-    'no-debugger': 'warn' // 禁止出现debugger
+    'no-debugger': 'warn', // 禁止出现debugger
+    // Allow @ts-ignore comment
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-unused-vars': 1,
+    '@typescript-eslint/no-empty-function': 1,
+    '@typescript-eslint/no-explicit-any': 0,
+    'prettier/prettier': 1,
+    // Vue: Recommended rules to be closed or modify
+    'vue/require-default-prop': 0,
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/max-attributes-per-line': 0,
+    // Vue: Add extra rules
+    'vue/custom-event-name-casing': [2, 'camelCase'],
+    'vue/no-v-text': 1,
+    'vue/padding-line-between-blocks': 1,
+    'vue/require-direct-export': 1,
+    'vue/multi-word-component-names': 0,
+    'import/extensions': [
+      2,
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+    'no-param-reassign': 0,
+    'prefer-regex-literals': 0,
+    'import/no-extraneous-dependencies': 0
   }
 }
