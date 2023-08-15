39d700b1c7f59fbcefdf8fbb3a1578b29ab0fc08
Alicehhhmm <1245500390@qq.com>
2023-08-15 13:02:00 +0800

    add:layout comp

--- src/layout/NavBar/index.vue
+++ src/layout/NavBar/index.vue
@@ -1,18 +1,89 @@
 <template>
-  <a-layout-footer class="footer">Navbar
-    
-  </a-layout-footer>
+  <a-layout-header class="navbar">
+    <div class="left-side">
+      <a-space>
+        <img
+          alt="logo"
+          src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/dfdba5317c0c20ce20e64fac803d52bc.svg~tplv-49unhts6dw-image.image"
+        />
+        <a-typography-title
+          :style="{ margin: 0, fontSize: '18px' }"
+          :heading="5"
+        >
+          Norush Pro
+        </a-typography-title>
+        <icon-menu-fold
+          style="font-size: 22px; cursor: pointer"
+        />
+      </a-space>
+    </div>
+    <div class="center-side">
+      <Menu />
+    </div>
+    <div class="right-side">Navbar right</div>
+  </a-layout-header>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
 
-<style lang="less" scoped>
-  .footer {
+</script>
+
+<style scoped lang="less">
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    height: 100%;
+    background-color: var(--color-bg-2);
+    border-bottom: 1px solid var(--color-border);
+  }
+
+  .left-side {
     display: flex;
     align-items: center;
-    justify-content: center;
-    height: 40px;
-    color: var(--color-text-2);
-    text-align: center;
+    padding-left: 20px;
+  }
+
+  .center-side {
+    flex: 1;
+  }
+
+  .right-side {
+    display: flex;
+    padding-right: 20px;
+    list-style: none;
+    :deep(.locale-select) {
+      border-radius: 20px;
+    }
+    li {
+      display: flex;
+      align-items: center;
+      padding: 0 10px;
+    }
+
+    a {
+      color: var(--color-text-1);
+      text-decoration: none;
+    }
+    .nav-btn {
+      border-color: rgb(var(--gray-2));
+      color: rgb(var(--gray-8));
+      font-size: 16px;
+    }
+    .trigger-btn,
+    .ref-btn {
+      position: absolute;
+      bottom: 14px;
+    }
+    .trigger-btn {
+      margin-left: 14px;
+    }
+  }
+</style>
+
+<style lang="less">
+  .message-popover {
+    .arco-popover-content {
+      margin-top: 0;
+    }
   }
 </style>
--- src/layout/default-layout.vue
+++ src/layout/default-layout.vue
@@ -1,180 +1,122 @@
 <template>
-  <a-layout class="layout-demo">
-    <a-layout-sider
-      theme="dark"
-      breakpoint="lg"
-      :width="220"
-      collapsible
-      :collapsed="collapsed"
-      @collapse="onCollapse"
-    >
-      <div class="logo" />
-      <a-menu
-        :defaultOpenKeys="['1']"
-        :defaultSelectedKeys="['0_2']"
-        @menuItemClick="onClickMenuItem"
-      >
-        <a-menu-item key="0_1" disabled>
-          <IconHome />
-          Menu 1
-        </a-menu-item>
-        <a-menu-item key="0_2">
-          <IconCalendar />
-          Menu 2
-        </a-menu-item>
-        <a-sub-menu key="1">
-          <template #title>
-            <span><IconCalendar />Navigation 1</span>
-          </template>
-          <a-menu-item key="1_1">Menu 1</a-menu-item>
-          <a-menu-item key="1_2">Menu 2</a-menu-item>
-          <a-sub-menu key="2" title="Navigation 2">
-            <a-menu-item key="2_1">Menu 1</a-menu-item>
-            <a-menu-item key="2_2">Menu 2</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="3" title="Navigation 3">
-            <a-menu-item key="3_1">Menu 1</a-menu-item>
-            <a-menu-item key="3_2">Menu 2</a-menu-item>
-            <a-menu-item key="3_3">Menu 3</a-menu-item>
-          </a-sub-menu>
-        </a-sub-menu>
-        <a-sub-menu key="4">
-          <template #title>
-            <span><IconCalendar />Navigation 4</span>
-          </template>
-          <a-menu-item key="4_1">Menu 1</a-menu-item>
-          <a-menu-item key="4_2">Menu 2</a-menu-item>
-          <a-menu-item key="4_3">Menu 3</a-menu-item>
-        </a-sub-menu>
-      </a-menu>
-    </a-layout-sider>
+  <a-layout class="layout">
+    <div class="layout-navbar">
+      <NavBar />
+    </div>
     <a-layout>
-      <a-layout-header>
-        <a-menu
-          :openKeys="['1']"
-          :selectedKeys="['0_2']"
-          mode='horizontal'
+      <a-layout>
+        <a-layout-sider 
+          style="width: 260px;" 
+          class="layout-sider"
+          breakpoint="xl"
+          :collapsed="collapsed"
+          :collapsible="true"
+          :hide-trigger="true"
+          :resize-directions="['right']"
         >
-          <a-menu-item key="0_1" disabled>
-            <IconHome />
-            Menu 1
-          </a-menu-item>
-          <a-menu-item key="0_2">
-            <IconCalendar />
-            Menu 2
-          </a-menu-item>
-          <a-sub-menu key="1">
-            <template #title>
-              <span><IconCalendar />Navigation 1</span>
-            </template>
-            <a-menu-item key="1_1">Menu 1</a-menu-item>
-            <a-menu-item key="1_2">Menu 2</a-menu-item>
-            <a-sub-menu key="2" title="Navigation 2">
-              <a-menu-item key="2_1">Menu 1</a-menu-item>
-              <a-menu-item key="2_2">Menu 2</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="3" title="Navigation 3">
-              <a-menu-item key="3_1">Menu 1</a-menu-item>
-              <a-menu-item key="3_2">Menu 2</a-menu-item>
-              <a-menu-item key="3_3">Menu 3</a-menu-item>
-            </a-sub-menu>
-          </a-sub-menu>
-          <a-sub-menu key="4">
-            <template #title>
-              <span><IconCalendar />Navigation 4</span>
-            </template>
-            <a-menu-item key="4_1">Menu 1</a-menu-item>
-            <a-menu-item key="4_2">Menu 2</a-menu-item>
-            <a-menu-item key="4_3">Menu 3</a-menu-item>
-          </a-sub-menu>
-        </a-menu>
-      </a-layout-header>
-      <a-layout style="padding: 0 24px">
-        <a-breadcrumb :style="{ margin: '16px 0' }">
-          <a-breadcrumb-item>Home</a-breadcrumb-item>
-          <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
-        </a-breadcrumb>
-        <a-layout-content>Content</a-layout-content>
-        <a-layout-footer>Footer</a-layout-footer>
+          <div class="menu-wrapper">
+            <Menu />
+          </div>
+        </a-layout-sider>
+        <!-- 侧边栏 -->
+        <a-drawer 
+          placement="left"
+          :footer="false"
+          mask-closable
+          :closable="false"
+        >
+          <Menu />
+        </a-drawer>
+        <!-- 主体内容 -->
+        <a-layout class="layout-content" >
+          <TabBar />
+          <a-layout-content>
+            <PageLayout />
+          </a-layout-content>
+          <Footer />
+        </a-layout>
+        
       </a-layout>
     </a-layout>
   </a-layout>
 </template>
 
-<script lang="ts" >
-import { defineComponent, ref } from 'vue'
-import { Message } from '@arco-design/web-vue'
-import {
-  IconHome,
-  IconCalendar,
-} from '@arco-design/web-vue/es/icon'
+<script lang="ts" setup>
+  import { ref } from "vue"
+  const collapsed = ref(false);
+  const onCollapse = () => {
 
-export default defineComponent({
-  components: {
-    IconHome,
-    IconCalendar,
-  },
-  setup() {
-    const collapsed = ref(false)
-    const onCollapse = (val:any, type:any) => {
-      const content = type === 'responsive' ? '触发响应式收缩' : '点击触发收缩'
-      Message.info({
-        content,
-        duration: 2000,
-      })
-      collapsed.value = val
+  }
+</script>
+
+<style scoped lang="less">
+  @nav-size-height: 60px;
+  @layout-max-width: 1100px;
+
+  .layout {
+    width: 100%;
+    height: 100%;
+  }
+
+  .layout-navbar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 100;
+    width: 100%;
+    height: @nav-size-height;
+  }
+
+  .layout-sider {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 99;
+    height: 100%;
+    transition: all 0.2s cubic-bezier(0.34, 0.69, 0.1, 1);
+    &::after {
+      position: absolute;
+      top: 0;
+      right: -1px;
+      display: block;
+      width: 1px;
+      height: 100%;
+      background-color: var(--color-border);
+      content: '';
     }
-    return {
-      collapsed,
-      onCollapse,
-      onClickMenuItem(key:any) {
-        Message.info({ content: `You select ${key}`, showIcon: true })
+
+    > :deep(.arco-layout-sider-children) {
+      overflow-y: hidden;
+    }
+  }
+
+  .menu-wrapper {
+    height: 100%;
+    overflow: auto;
+    overflow-x: hidden;
+    :deep(.arco-menu) {
+      ::-webkit-scrollbar {
+        width: 12px;
+        height: 4px;
+      }
+
+      ::-webkit-scrollbar-thumb {
+        border: 4px solid transparent;
+        background-clip: padding-box;
+        border-radius: 7px;
+        background-color: var(--color-text-4);
+      }
+
+      ::-webkit-scrollbar-thumb:hover {
+        background-color: var(--color-text-3);
       }
     }
   }
-})
-</script>
-<style scoped>
-.layout-demo {
-  height: 500px;
-  background: var(--color-fill-2);
-  border: 1px solid var(--color-border);
-}
-.layout-demo :deep(.arco-layout-sider) .logo {
-  height: 32px;
-  margin: 12px 8px;
-  background: rgba(255, 255, 255, 0.2);
-}
-.layout-demo :deep(.arco-layout-sider-light) .logo{
-  background: var(--color-fill-2);
-}
-.layout-demo :deep(.arco-layout-header)  {
-  height: 64px;
-  line-height: 64px;
-  background: var(--color-bg-3);
-}
-.layout-demo :deep(.arco-layout-footer) {
-  height: 48px;
-  color: var(--color-text-2);
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 48px;
-}
-.layout-demo :deep(.arco-layout-content) {
-  color: var(--color-text-2);
-  font-weight: 400;
-  font-size: 14px;
-  background: var(--color-bg-3);
-}
-.layout-demo :deep(.arco-layout-footer),
-.layout-demo :deep(.arco-layout-content)  {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  color: var(--color-white);
-  font-size: 16px;
-  font-stretch: condensed;
-  text-align: center;
-}
+
+  .layout-content {
+    min-height: 100vh;
+    overflow-y: hidden;
+    background-color: var(--color-fill-2);
+    transition: padding 0.2s cubic-bezier(0.34, 0.69, 0.1, 1);
+  }
 </style>
